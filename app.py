f5d6f3c467c51e280bc88ae1d83718caa077f768
Adam Xu <adam.yi.xu@gmail.com>
2024-08-02 15:07:34 -0700

    eating food 23422442

--- app.py
+++ app.py
@@ -161,4 +161,4 @@ def rules():
     return render_template('rules.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(debug=False, port=80, host="0.0.0.0")
